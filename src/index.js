9d70606ed0f7dddb49a442e82ed6f7e347417f9a
cmsxi <cms0755@naver.com>
2024-08-19 12:05:31 +0900

    feat: Add attendance calendar and post type stamps display

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./assets/index.css";
-import App from "./App";
+import ViewAllAttendance from "./pages/ViewAllAttendance"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<React.StrictMode>
+    <ViewAllAttendance/>
+</React.StrictMode>);
