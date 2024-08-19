e6ae60d87c755878d064ba1ea5c94cdd4263446f
soyoungxxx <otherplet@gmail.com>
2024-08-19 14:48:05 +0900

    fix: Fix conflict

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./assets/index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/index.css';
+import ViewAllAttendance from './pages/attendance/ViewAllAttendance';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <React.StrictMode>
+        <ViewAllAttendance />
+    </React.StrictMode>,
+);
