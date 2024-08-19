8853d8ebf9318dec4732bc07f9618a712896e6cb
soyoungxxx <otherplet@gmail.com>
2024-08-19 15:35:09 +0900

    style: Seperate Common Component
    - 자주 쓰이는 Container를 컴포넌트로 분리
    - 공용 컴포넌트로 지정
    - CSS 분리 및 코드 정리

--- src/components/main/SavingsMain.js
+++ src/components/main/SavingsMain.js
@@ -5,14 +5,15 @@ import React from 'react';
 import '../../assets/mainPage/MainPage.css';
 import '../../assets/mainPage/ActiveSavingsMain.css';
 import '../../assets/mainPage/NoSavingsMain.css';
+import Container from '../common/Container';
 
-const SavingsMain = ({ containerClass, buttonClass, imgSrc }) => {
+const SavingsMain = ({ buttonClass, imgSrc }) => {
     return (
-        <div className={`mainContainer ${containerClass}`}>
+        <Container>
             <img src={imgSrc} alt="mainImage" className="mainBackground" />
             <button className="attendanceBtn mainBtn"></button>
             <button className={`mainBtn ${buttonClass}`}></button>
-        </div>
+        </Container>
     );
 };
 
