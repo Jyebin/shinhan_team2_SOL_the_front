e1e66fafd8bd9496fb877aac0fa84839794f3693
soyoungxxx <otherplet@gmail.com>
2024-08-19 10:44:33 +0900

    design: edit the main page frame
    - 배경 이미지 고정 사이즈로 수정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import '../assets/mainPage/MainPage.css';
-import mainEmptyURL from '../assets/mainPage/main_empty.png';
 
 const MainPage = () => {
-    return (
-        <div className="mobileViewContainer">
-            <img src={mainEmptyURL} alt="Main View" className="mainEmpty_img" />
-        </div>
-    );
+    return <div className="mobileViewContainer"></div>;
 };
 
 export default MainPage;
