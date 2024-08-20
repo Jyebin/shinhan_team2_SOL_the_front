4d4136495e2f61b1b3bb9e132e6ea8ed380d70f3
soyoungxxx <otherplet@gmail.com>
2024-08-20 18:56:11 +0900

    feat: Add Css file
    - Css 파일 추가
    - Container에 스타일 적용

--- src/pages/MyCanPage.js
+++ src/pages/MyCanPage.js
@@ -1,9 +1,17 @@
 import Container from '../components/common/Container';
 import canUrl from '../assets/common/img/can_img.png';
+import '../assets/myCan/MyCanPage.css';
 
 const MyCanPage = () => {
     return (
-        <Container>
+        <Container
+            style={{
+                backgroundColor: '#f3f6fb',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+            }}
+        >
             <div className="bucket-content">
                 <h2 className="content-title">깡통에 얼마나 모였을까요?</h2>
                 <p className="content-subtitle">깡통을 눌러 뒤집어주세요</p>
