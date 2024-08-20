117a54db828ed5e6373f8ed7cc6db938fe2977d1
soyoungxxx <otherplet@gmail.com>
2024-08-20 18:39:04 +0900

    design: Complete
    - 디자인 조절 완료

--- src/components/depositHistory/Transaction.js
+++ src/components/depositHistory/Transaction.js
@@ -2,18 +2,23 @@ import React from 'react';
 import '../../assets/depositHistoryPage/Transaction.css';
 
 const Transaction = ({ transaction }) => {
+    const amountStatus =
+        transaction.status === '입금' ? 'amount-positive' : 'amount-negative';
     return (
         <div className="transaction">
             <span className="transaction-date">{transaction.date}</span>
             <span className="transaction-time"> {transaction.time}</span>
+            <span className={`deposit-status ${amountStatus}`}>
+                {transaction.status}
+            </span>
             <div className="transaction-info">
                 <span className="description">{transaction.description}</span>
-                <span className="amount">
+                <span className={`amount ${amountStatus}`}>
                     {transaction.amount.toLocaleString()}원
                 </span>
             </div>
             <div className="balance-info">
-                잔액 {transaction.balance.toLocaleString()}원
+                {transaction.balance.toLocaleString()}원
             </div>
         </div>
     );
