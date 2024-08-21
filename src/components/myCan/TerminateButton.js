8c58bcfed562c58c504dcc4731c12a2f867cc612
soyoungxxx <otherplet@gmail.com>
2024-08-21 13:56:40 +0900

    refactor: Seperate component
    - 컴포넌트 분리

---
+++ src/components/myCan/TerminateButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../assets/myCan/TerminateButton.css';
+
+const TerminateButton = () => {
+    return (
+        <div className="button-container">
+            <button className="terminate-button">깡통 해지하기</button>
+        </div>
+    );
+};
+
+export default TerminateButton;
