d8df33a5de608d91dab97494638b327a9bac6d1b
Jyebin <yebin0322@naver.com>
2024-08-21 14:06:30 +0900

    feat : Modify page movement - change from '/' to '/attendance/main'

--- src/components/navigator/Header.js
+++ src/components/navigator/Header.js
@@ -45,13 +45,18 @@ function Header({ onBack, onCancel, step }) {
         }
     };
 
+    const handleBackClick = () => {
+        if (location.pathname === '/attendance/post') {
+            navigate('/attendance/main');
+        } else {
+            onBack(navigate);
+        }
+    };
+
     return (
         <header className="header">
             {step < 5 && ( // step이 5가 아닐 때만 뒤로가기 버튼을 표시
-                <button
-                    className="header-button"
-                    onClick={() => onBack(navigate)}
-                >
+                <button className="header-button" onClick={handleBackClick}>
                     &lt;
                 </button>
             )}
