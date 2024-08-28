1a9f65b971222706d6d00a778521e20132bce934
soyoungxxx <otherplet@gmail.com>
2024-08-28 12:22:37 +0900

    refactor: Remove unnecessary status variables
    - isInitialized 제거

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,25 +4,17 @@ import NoSavings from '../components/main/NoSavings';
 import { SavingsContext } from '../store/SavingsProvider';
 
 const MainPage = () => {
-    const {
-        hasSavings: state,
-        isInitialized,
-        initialize,
-    } = useContext(SavingsContext);
+    const { hasSavings, initialize } = useContext(SavingsContext);
 
     useEffect(() => {
-        initialize(); // 메인 페이지에 처음 접근할 때만 초기화
+        initialize(); // 메인 페이지에 처음 접근할 때 초기화
     }, [initialize]);
 
-    if (!isInitialized) {
-        return <p>Loading...</p>; // 초기화 중일 때 로딩 표시
-    }
-
-    if (state === null) {
+    if (hasSavings === null) {
         return <div>Loading...</div>;
     }
 
-    return <div>{state ? <ActiveSavings /> : <NoSavings />}</div>;
+    return <div>{hasSavings ? <ActiveSavings /> : <NoSavings />}</div>;
 };
 
 export default MainPage;
